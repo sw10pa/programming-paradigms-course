f37cc890b81cff951de53b81397b832e11b6bef2
Broxigar <58048604+d-chechelashvili@users.noreply.github.com>
2020-07-31 23:37:01 +0400

    Added some functional for lecturer

--- src/main/java/PPC/controller/LectureHandler.java
+++ src/main/java/PPC/controller/LectureHandler.java
@@ -1,17 +1,32 @@
 package PPC.controller;
 
+import PPC.database.PPCDatabase;
+import PPC.database.PPCDatabaseManager;
+import PPC.filesystem.FileManager;
+import PPC.model.Lecture;
 import PPC.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @Controller
 public class LectureHandler {
+
+    private final PPCDatabaseManager dbManager;
+
+    public LectureHandler() throws SQLException, ClassNotFoundException {
+        PPCDatabase ppcDatabase = new PPCDatabase();
+        dbManager = new PPCDatabaseManager(ppcDatabase.getConnection());
+    }
 
     @GetMapping("/lecture")
     public ModelAndView get(HttpServletResponse resp, HttpSession ses, HttpServletRequest req) throws IOException {
@@ -27,6 +42,30 @@ public class LectureHandler {
         String lectureId = req.getParameter("lectureId");
         ret.addObject("lectureId", lectureId);
 
+        return ret;
+    }
+
+    @PostMapping("/edit-link")
+    public void editVideoUrl(HttpServletResponse resp,
+                             @RequestParam String lectureId,
+                             @RequestParam String videoURL) throws IOException, SQLException {
+        dbManager.setLectureVideoUrl(Integer.parseInt(lectureId), videoURL);
+        resp.sendRedirect("/lecture");
+    }
+
+    @PostMapping("/edit-text")
+    public void editLectureText(HttpServletResponse resp,
+                                @RequestParam String lectureId,
+                                @RequestParam String newText) throws SQLException, IOException {
+        Lecture lec = dbManager.getLectureById(Integer.parseInt(lectureId));
+        FileManager.writeToFile(Lecture.LECTURES_FILES_PATH, lec.getFileName(), newText);
+        resp.sendRedirect("/lecture");
+    }
+
+    @GetMapping("/edit-quiz")
+    public ModelAndView editQuiz(@RequestParam String lectureId) {
+        ModelAndView ret = new ModelAndView("add-question");
+        ret.addObject("lectureId", lectureId);
         return ret;
     }
 
---
+++ src/main/webapp/WEB-INF/view/add-question.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Whiskeyjack
+  Date: 31-Jul-20
+  Time: 23:31
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Add Question</title>
+</head>
+<body>
+
+</body>
+</html>
